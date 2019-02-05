37e269179d8a3a2ba119dea84abd490296e581fb
Gangapatnam,Vamsee Krishna <s533908@ch140027m.nwmissouri.edu>
2019-02-05 10:49:19 -0600

    Project complete wohooo

--- TIme Converter/ViewController.swift
+++ TIme Converter/ViewController.swift
@@ -10,6 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var yearsTF: UITextField!
+    
+    @IBOutlet weak var monthsTF: UILabel!
+    
+    @IBOutlet weak var daysTF: UILabel!
+    
+    
+    @IBOutlet weak var secondsTF: UILabel!
+    
+    
+  
+    @IBAction func convertIt(_ sender: Any) {
+        let years = Int(yearsTF.text!)!
+        let months = years * 12
+        let days = years * 365
+        let seconds = days * 24 * 60 * 60
+        monthsTF.text = String(months)
+        daysTF.text = String(days)
+        secondsTF.text = String(seconds)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.

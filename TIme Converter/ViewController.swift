c3b1e2eab97732b711e9a62a2e84fc80144c8f00
Gangapatnam,Vamsee Krishna <s533908@ch140027m.nwmissouri.edu>
2019-02-05 09:48:30 -0600

    Initial Commit

---
+++ TIme Converter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TIme Converter
+//
+//  Created by Gangapatnam,Vamsee Krishna on 2/5/19.
+//  Copyright © 2019 Gangapatnam,Vamsee Krishna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
